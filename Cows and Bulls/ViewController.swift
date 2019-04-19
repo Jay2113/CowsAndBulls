bdbd2348a13a5d55b0c3e2f071c69441137fbc10
Jay <jay.ambrish.raval@gmail.com>
2019-04-19 19:01:50 -0400

    Filling in the table view.

--- Cows and Bulls/ViewController.swift
+++ Cows and Bulls/ViewController.swift
@@ -8,28 +8,43 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
 
     @IBOutlet var tableView: NSTableView!
     @IBOutlet var guess: NSTextField!
-    
+    var answer = ""
+    var guesses = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return guesses.count
+    }
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+    func result(for guess: String) -> String {
+        return "Result"
     }
     
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else { return nil }
+        if tableColumn?.title == "Guess" {
+            // this is the "Guess" column; show a previous guess
+            vw.textField?.stringValue = guesses[row]
+        } else {
+            // this is the "Result" column; call our new method
+            vw.textField?.stringValue = result(for: guesses[row])
+        }
+        return vw
+    }
+    
+    func tableView(_ tableView: NSTableView, shouldSelectRow row: Int) -> Bool {
+        return false
+    }
     
     @IBAction func submitGuess(_ sender: Any) {
     }
-    
-
 }
-
