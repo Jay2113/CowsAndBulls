55ed749c3244301ba5560a21bded4d1e01d291d5
Jay <jay.ambrish.raval@gmail.com>
2019-04-19 18:05:41 -0400

    Initial Commit

---
+++ Cows and Bulls/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Cows and Bulls
+//
+//  Created by Jay Raval on 4/19/19.
+//  Copyright © 2019 Jay Raval. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Cows and Bulls/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Cows and Bulls
+//
+//  Created by Jay Raval on 4/19/19.
+//  Copyright © 2019 Jay Raval. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
