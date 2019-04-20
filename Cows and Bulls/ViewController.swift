21daed2019e7300e7febf49eef12fb3bbf6d9ce4
Jay <jay.ambrish.raval@gmail.com>
2019-04-20 00:23:18 -0400

    Marking the guesses

--- Cows and Bulls/ViewController.swift
+++ Cows and Bulls/ViewController.swift
@@ -25,7 +25,18 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
     }
 
     func result(for guess: String) -> String {
-        return "Result"
+        var bulls = 0
+        var cows = 0
+        let guessLetters = Array(guess)
+        let answerLetters = Array(answer)
+        for (index, letter) in guessLetters.enumerated() {
+            if letter == answerLetters[index] {
+                bulls += 1
+            } else if answerLetters.contains(letter) {
+                cows += 1
+            }
+        }
+        return "\(bulls)b \(cows)c"
     }
     
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
@@ -45,6 +56,27 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
     }
     
     @IBAction func submitGuess(_ sender: Any) {
+        // check for 4 unique characters
+        let guessString = guess.stringValue
+        guard Set(guessString).count == 4 else { return }
+        guard guessString.count == 4 else { return }
+        
+        // ensure there are no non-digit characters
+        let badCharacters = CharacterSet(charactersIn: "0123456789").inverted
+        guard guessString.rangeOfCharacter(from: badCharacters) == nil else { return }
+        
+        // add the guess to the array and table view
+        guesses.insert(guessString, at: 0)
+        tableView.insertRows(at: IndexSet(integer: 0), withAnimation: .slideDown)
+        
+        // did the player win?
+        let resultString = result(for: guessString)
+        if resultString.contains("4b")
+        let alert = NSAlert()
+        alert.messageText = "You win"
+        alert.informativeText = "Congratulations! Click OK to play again."
+        alert.runModal()
+        startNewGame()
     }
     
     func startNewGame() {
